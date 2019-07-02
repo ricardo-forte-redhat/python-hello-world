9162203cdf5d056bccdac0ae45f268ebd792a659
Ricardo Forte <rforte@redhat.com>
2019-07-01 22:51:42 -0300

    Yet another try, almost good

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 class HelloRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path != '/':
-            self.send_error(404, "Object not found")
-            return
+        # if self.path != '/':
+        #     self.send_error(404, "Object not found")
+        #     return
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
