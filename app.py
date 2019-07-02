f125d6e7613d9d38b3400f3262fdb9834e5d795a
Ricardo Forte <rforte@redhat.com>
2019-07-01 22:54:51 -0300

    The port was wrong

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 class HelloRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        # if self.path != '/':
-        #     self.send_error(404, "Object not found")
-        #     return
+        if self.path != '/':
+            self.send_error(404, "Object not found")
+            return
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
@@ -28,6 +28,6 @@ class HelloRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response_text.encode('utf-8'))
 
 
-server_address = ('', 8000)
+server_address = ('', 8080)
 httpd = HTTPServer(server_address, HelloRequestHandler)
 httpd.serve_forever()
